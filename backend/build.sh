d9d66c7f70a2a0355d857875504f7e70543efc9c
[Shrey Parikh] <[parikhshrey077@gmail.com]>
2026-07-21 19:03:04 +0530

    Forcefully reset admin password

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else print('Admin already exists')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user, created = User.objects.get_or_create(username='admin'); user.set_password('admin123'); user.is_staff = True; user.is_superuser = True; user.save(); print('Admin password forcefully reset to admin123')"
 python manage.py shell < seed.py
